ddca2073ed6d9f7f4018f7f9533997375ed78d4d
arzukrts <101825128+arzukrts@users.noreply.github.com>
2023-12-26 14:27:09 +0300

    Update eemGirisKod.ino

--- eemGiris/eemGirisKod.ino
+++ eemGiris/eemGirisKod.ino
@@ -2,57 +2,57 @@
 #include <MFRC522.h>                      //MFRC522 kütüphanemizi tanımlıyoruz.
 #include <Servo.h>                        //Servo kütüphanemizi tanımlıyoruz.
 
-int RST_PIN = 9;                          //RC522 modülü reset pinini tanımlıyoruz.
-int SS_PIN = 10;                          //RC522 modülü chip select pinini tanımlıyoruz.
-int servoPin = 8;                         //Servo motor pinini tanımlıyoruz.
+int RST_PIN = 9;                         
+int SS_PIN = 10;                         
+int servoPin = 8;                        
 Servo motor;
-MFRC522 rfid(SS_PIN, RST_PIN);            //RC522 modülü ayarlarını yapıyoruz.s
-byte ID[4] = {114, 33, 62, 81};          //Yetkili kart ID'sini tanımlıyoruz. 
+MFRC522 rfid(SS_PIN, RST_PIN);           
+byte ID[4] = {114, 33, 62, 81};         
 
 int redLEDPin = 6;
 int greenLEDPin = 3;
 
 void setup() { 
-  motor.attach(servoPin);                 //Servo motor pinini motor değişkeni ile ilişkilendiriyoruz.
-  Serial.begin(9600);                     //Seri haberleşmeyi başlatıyoruz.
-  SPI.begin();                            //SPI iletişimini başlatıyoruz.
-  rfid.PCD_Init();                        //RC522 modülünü başlatıyoruz.
+  motor.attach(servoPin);
+  Serial.begin(9600);                    
+  SPI.begin();                           
+  rfid.PCD_Init();                       
 
-  pinMode(redLEDPin, OUTPUT);    // Kırmızı LED'i çıkış olarak ayarla
-  pinMode(greenLEDPin, OUTPUT);  // Yeşil LED'i çıkış olarak ayarla
-  digitalWrite(redLEDPin, LOW);  // Kırmızı LED'i başlangıçta kapalı yap
-  digitalWrite(greenLEDPin, LOW); // Yeşil LED'i başlangıçta kapalı yap
+  pinMode(redLEDPin, OUTPUT);    
+  pinMode(greenLEDPin, OUTPUT);  
+  digitalWrite(redLEDPin, LOW);  
+  digitalWrite(greenLEDPin, LOW); 
 
 }
  
 void loop() {
 
-  if ( ! rfid.PICC_IsNewCardPresent())    //Yeni kartın okunmasını bekliyoruz.
+  if ( ! rfid.PICC_IsNewCardPresent())  
     return;
 
-  if ( ! rfid.PICC_ReadCardSerial())      //Kart okunmadığı zaman bekliyoruz.
+  if ( ! rfid.PICC_ReadCardSerial())      
     return;
 
-  if (rfid.uid.uidByte[0] == ID[0] &&     //Okunan kart ID'si ile ID değişkenini karşılaştırıyoruz.
+  if (rfid.uid.uidByte[0] == ID[0] &&   
     rfid.uid.uidByte[1] == ID[1] && 
     rfid.uid.uidByte[2] == ID[2] && 
     rfid.uid.uidByte[3] == ID[3] ) {
         Serial.println("Kapi acildi");
         ekranaYazdir();
         digitalWrite(greenLEDPin, HIGH);
-        motor.write(100);                 //Servo motoru 180 dereceye getiriyoruz.
+        motor.write(100);                
         delay(2000);
-        motor.write(0);                   //Servo motoru 0 dereceye getiriyoruz.
+        motor.write(0);                  
         delay(500);
         digitalWrite(greenLEDPin, LOW);
 
     }
-    else{                                 //Yetkisiz girişte içerideki komutlar çalıştırılır.
+    else{                                
       Serial.println("Yetkisiz Kart");
       ekranaYazdir();
-      digitalWrite(redLEDPin, HIGH);    // Kırmızı LED'i yak
+      digitalWrite(redLEDPin, HIGH);
       delay(500);
-      digitalWrite(redLEDPin, LOW);     // Kırmızı LED'i kapat
+      digitalWrite(redLEDPin, LOW);     
     }
   rfid.PICC_HaltA();
 }
